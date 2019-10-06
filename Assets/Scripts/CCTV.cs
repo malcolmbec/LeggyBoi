3db6ec4f19c935b089ad111ab0f3628324f8a992
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2019-10-06 16:33:03 -0400

    Created CCTV camera prefab with animated radius

---
+++ Assets/Scripts/CCTV.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCTV : MonoBehaviour
+{
+    public Color triggerColor;
+
+    private SpriteRenderer spr;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        anim = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision!");
+            anim.enabled = false;
+            spr.color = triggerColor;
+        }
+    }
+}
