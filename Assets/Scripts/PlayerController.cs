1e2c9fa4274bbb5f5fa198b98cf5ef27cc477fbf
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2019-10-06 14:36:25 -0400

    Basic player movement, set boundaries on level map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(moveX * speed, moveY * speed);
     }
 }
