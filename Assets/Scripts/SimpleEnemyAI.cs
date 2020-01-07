6d5dfbec0286616bbe80fb6ed2830b65312b9736
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2020-01-07 14:40:22 -0500

    untested enemy ai, going on hiatus

---
+++ Assets/Scripts/SimpleEnemyAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemyAI : MonoBehaviour
+{
+    public GameObject waypoint1;
+    public GameObject waypoint2;
+    public float travelDuration;
+    public float wait;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(moveBetweenWaypoints());
+    }
+
+    // Moves constantly between waypoints, waiting inbetween
+    public IEnumerator moveBetweenWaypoints()
+    {
+        yield return StartCoroutine(moveToWaypoint(waypoint1));
+        yield return new WaitForSeconds(wait);
+        yield return StartCoroutine(moveToWaypoint(waypoint2));
+        yield return new WaitForSeconds(wait);
+        yield return null;
+    }
+
+    // Moves to a waypoint over a duration of time
+    public IEnumerator moveToWaypoint(GameObject waypoint)
+    {
+        float counter = 0f;
+        Vector2 waypointPos = waypoint.transform.position;
+        while (counter < travelDuration)
+        {
+            transform.position = Vector2.Lerp(transform.position, waypointPos, counter / travelDuration);
+            counter += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = waypointPos;
+    }
+
+}
